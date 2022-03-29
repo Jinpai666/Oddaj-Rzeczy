bcdb3770e940f945656ae40bb2b34d1449626c4f
Jinpai666 <41897625+Jinpai666@users.noreply.github.com>
2022-03-29 18:47:43 +0200

    about done

--- src/components/HomeAbout.js
+++ src/components/HomeAbout.js
@@ -1,11 +1,15 @@
 import React from "react";
+import DecoratedHeader from "./DecoratedHeader";
 
 
 export default function HomeAbout(){
     return (
         <section className="about">
-            <div className="about__left-side"></div>
-            <div className="about__image"></div>
+            <div className="about__left-side">
+                <DecoratedHeader text={"O nas"} styling={"about__header"}/>
+                <p className="about__text">Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+            </div>
+
         </section>
     )
 }
--- src/components/HomeMainSection.js
+++ src/components/HomeMainSection.js
@@ -7,7 +7,7 @@ export default function HomeMainSection(){
 
     return (
         <section id="main" className="main-section">
-            <div className="main-section__img"></div>
+            {/*<div className="main-section__img"></div>*/}
             <div className="main-section__right-col">
                 <h1 className="main-section__heading">Zacznij pomagać!</h1>
                 <DecoratedHeader styling={"main-section__heading-bottom"} text={"Oddaj niechciane rzeczy w zaufane ręce"} />
