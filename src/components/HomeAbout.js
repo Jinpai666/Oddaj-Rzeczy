1573bcccf73b0dc97d614574e999133b3819c795
Jinpai666 <41897625+Jinpai666@users.noreply.github.com>
2022-03-29 17:38:11 +0200

    about started

--- src/components/HomeAbout.js
+++ src/components/HomeAbout.js
@@ -3,6 +3,9 @@ import React from "react";
 
 export default function HomeAbout(){
     return (
-        <div>About</div>
+        <section className="about">
+            <div className="about__left-side"></div>
+            <div className="about__image"></div>
+        </section>
     )
 }
