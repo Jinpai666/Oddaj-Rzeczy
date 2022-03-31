b4de6402d3009ef775f0c4bb873c57e9f936ba87
Jinpai666 <41897625+Jinpai666@users.noreply.github.com>
2022-03-31 16:53:24 +0200

    pagination done

--- src/components/HomeHelp.js
+++ src/components/HomeHelp.js
@@ -1,38 +1,95 @@
 import React, {useState, useEffect} from "react";
 import DecoratedHeader from "./DecoratedHeader";
-import Foundations from "../collections/foundations.json"
-import Organisations from "../collections/organisations.json"
-import Local from "../collections/local.json"
+import Foundations from "../collections/foundations.json";
+import Organisations from "../collections/organisations.json";
+import Local from "../collections/local.json";
+import ReactPaginate from 'react-paginate';
 
 export default function HomeHelp(){
-    const foundations = Foundations.slice(0,3);
-    const organisations = Organisations.slice(0,3);
-    const locals = Local.slice(0,3);
+    //change collection
     const [clicked, setClicked] = useState('Fundacjom')
-    // const [dataJson, setDataJson] = useState(foundations)
+    const [dataJson, setDataJson] = useState(Foundations)
     const handleClick = (e) => {
         const collection = e.target.innerHTML;
         setClicked(collection);
     }
-
-    // useEffect(() =>{
-    //     if (clicked==="Fundacjom"){
-    //         setDataJson(foundations)
-    //     }else if(clicked==="Lokalnym zbiórkom"){
-    //         setDataJson(local)
-    //     }else if(clicked==="Organizacjom pozarządowym"){
-    //         setDataJson(organisations)
-    //     }
-    // }, [clicked, dataJson])
+    useEffect(() =>{
+        if (clicked==="Fundacjom"){
+            setDataJson(Foundations)
+        }else if(clicked==="Lokalnym zbiórkom"){
+            setDataJson(Local)
+        }else if(clicked==="Organizacjom pozarządowym"){
+            setDataJson(Organisations)
+        }
+    }, [clicked, dataJson])
+    //paginate
+    const [pageNr, setPageNr] = useState(0);
+    const groupsPerPage = 3;
+    const pagesVisited = pageNr * groupsPerPage;
+    //foundations
+    const displayFoundations = dataJson.slice(pagesVisited, pagesVisited + groupsPerPage).map(group => {
+        return (
+            <div className="help__details" key={group.id}>
+                <div className="help__detail help__detail-left" >
+                    <p className="help__detail-left-row-1" >Fundacja "{group.name}"</p>
+                    <p className="help__detail-left-row-2" >Cel i misja: {group.goals}.</p>
+                </div>
+                <p className="help__detail help__detail-right help__detail-row">{group.items}</p>
+            </div>
+        );
+    });
+    //organisations
+    const displayOrganisations = dataJson.slice(pagesVisited, pagesVisited + groupsPerPage).map(group => {
+        return (
+            <div className="help__details" key={group.id}>
+                <div className="help__detail help__detail-left" >
+                    <p className="help__detail-left-row-1" >Organizacja "{group.name}"</p>
+                    <p className="help__detail-left-row-2" >{group.goals}.</p>
+                </div>
+                <p className="help__detail help__detail-right help__detail-row">{group.items}</p>
+            </div>
+        );
+    });
+    //locals
+    const displayLocals = dataJson.slice(pagesVisited, pagesVisited + groupsPerPage).map(group => {
+        return (
+            <div className="help__details" key={group.id}>
+                <div className="help__detail help__detail-left" >
+                    <p className="help__detail-left-row-1" >Zbiórka "{group.name}"</p>
+                    <p className="help__detail-left-row-2" >{group.goals}.</p>
+                </div>
+                <p className="help__detail help__detail-right help__detail-row">{group.items}</p>
+            </div>
+        );
+    });
+    const pageCount = Math.ceil(dataJson.length / groupsPerPage);
+    const changePage = ({selected}) => {
+        setPageNr(selected)
+    }
 
 
     return (
         <section id="help" className="help">
             <DecoratedHeader styling={"help__header"} text={"Komu pomagamy?"} />
             <div className="help__buttons">
-                <div onClick={handleClick} className="help__button">Fundacjom</div>
-                <div onClick={handleClick} className="help__button">Organizacjom pozarządowym</div>
-                <div onClick={handleClick} className="help__button">Lokalnym zbiórkom</div>
+                <div
+                    onClick={handleClick}
+                    className="help__button"
+                    style={clicked==="Fundacjom" ? {border:"1px solid #1A1818"} : {}}
+                >Fundacjom
+                </div>
+                <div
+                    onClick={handleClick}
+                    className="help__button"
+                    style={clicked==="Organizacjom pozarządowym" ? {border:"1px solid #1A1818"} : {}}
+                >Organizacjom pozarządowym
+                </div>
+                <div
+                    onClick={handleClick}
+                    className="help__button"
+                    style={clicked==="Lokalnym zbiórkom" ? {border:"1px solid #1A1818"} : {}}
+                >Lokalnym zbiórkom
+                </div>
             </div>
             {clicked==="Fundacjom" && <div className="help__text">W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.
             </div>}
@@ -41,41 +98,17 @@ export default function HomeHelp(){
             {clicked==="Lokalnym zbiórkom" && <div className="help__text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, velit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis culpa  dignissimos ea minima quos.
             </div>}
 
-
-            {clicked==="Fundacjom" && foundations.map((foundation)=>{
-                return (
-                    <div className="help__details" key={foundation.id}>
-                        <div className="help__detail help__detail-left" >
-                            <p className="help__detail-left-row-1" >Fundacja "{foundation.name}"</p>
-                            <p className="help__detail-left-row-2" >Cel i misja: {foundation.goals}.</p>
-                        </div>
-                        <p className="help__detail help__detail-right help__detail-row">{foundation.items}</p>
-                    </div>
-                )
-            })}
-            {clicked==="Organizacjom pozarządowym" && organisations.map((organisation)=>{
-                return (
-                    <div className="help__details" key={organisation.id}>
-                        <div className="help__detail help__detail-left" >
-                            <p className="help__detail-left-row-1" >Organizacja "{organisation.name}"</p>
-                            <p className="help__detail-left-row-2" >{organisation.goals}.</p>
-                        </div>
-                        <p className="help__detail help__detail-right help__detail-row">{organisation.items}</p>
-                    </div>
-                )
-            })}
-            {clicked==="Lokalnym zbiórkom" && locals.map((local)=>{
-                return (
-                    <div className="help__details" key={local.id}>
-                        <div className="help__detail help__detail-left" >
-                            <p className="help__detail-left-row-1" >Zbiórka "{local.name}"</p>
-                            <p className="help__detail-left-row-2" >{local.goals}.</p>
-                        </div>
-                        <p className="help__detail help__detail-right help__detail-row">{local.items}</p>
-                    </div>
-                )
-            })}
-
+            {clicked==="Fundacjom" && displayFoundations}
+            {clicked==="Organizacjom pozarządowym" && displayOrganisations}
+            {clicked==="Lokalnym zbiórkom" && displayLocals}
+            {dataJson.length > 3 && <ReactPaginate
+                pageCount={pageCount}
+                containerClassName="help__pagination"
+                previousClassName="help__hide-buttons"
+                nextClassName="help__hide-buttons"
+                onPageChange={changePage}
+                pageLinkClassName="help__pagination-button"
+            />}
         </section>
     )
 }
