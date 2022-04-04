7433131acc3ebd61de9a8e4e4569ab199f94c3cc
Jinpai666 <41897625+Jinpai666@users.noreply.github.com>
2022-04-04 21:08:31 +0200

    change media queries for RWD optimalization

--- src/components/HomeFooter.js
+++ src/components/HomeFooter.js
@@ -4,8 +4,9 @@ import { FaFacebookSquare, FaInstagram } from 'react-icons/fa'
 export default function HomeFooter(){
     return (
         <section className="footer">
-            <h3 className="footer__header">Copyright by Coders Lab</h3>
-            <div className="footer__icons">
+            <div className="footer__item"></div>
+            <p className="footer__text footer__item">Copyright by Coders Lab</p>
+            <div className="footer__icons footer__item">
                 <a href="https://www.facebook.com/" target="_blank"><FaFacebookSquare/></a>
                 <a href="https://www.instagram.com/" target="_blank"><FaInstagram/></a>
             </div>
