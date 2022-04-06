653327a384de07c14c7ccb1db60552ba207be748
Jinpai666 <41897625+Jinpai666@users.noreply.github.com>
2022-04-06 20:22:44 +0200

    complete login layout, prepare logic for firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,16 +1,58 @@
-import React from "react";
-import {Link, useNavigate} from "react-router-dom";
+import React, { useState } from "react";
+
+import {Link} from "react-router-dom";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import DecoratedHeader from "./DecoratedHeader";
+import axios from "axios";
 
 
 export default function Login(){
-    // const formik = useFormik();
+    const[loginEmail, setLoginEmail] = useState("");
+    const[loginPassword, setLoginPassword] = useState("");
+    const changeEmailValue = (event) => {setLoginEmail(event.target.value)};
+    const changePasswordlValue = (event) => {setLoginPassword(event.target.value)};
+    //firebase data
+    // const[loginError, setLoginError] = useState('')
+    // const login = async () =>{
+    //     try {
+    //         const user = await signInWithEmailAndPassword(
+    //             auth,
+    //             loginEmail,
+    //             loginPassword
+    //         );
+    //         console.log(user)
+    //     } catch (error){
+    //         console.log(error.message)
+    //         setLoginError(error)
+    //     }
+    // }
+
+//formik data collection
+    const formik = useFormik({
+        initialValues:{
+            email: "",
+            password:""
+        },
+        //yup validation
+        validationSchema: Yup.object({
+            email: Yup.string()
+                .email("Niepoprawny adres email")
+                .required("Proszę podać adres email"),
+            password: Yup.string()
+                .min(6,"Hasło musi mieć przynajmniej 6 znaków")
+                .required("Proszę podać hasło"),
+        }),
+        //post on submit
+        onSubmit: () => {
+            console.log('loguje')
+        },
+    });
+
 
     return (
         <section className="login">
-            <div className="login__container">
+            <form className="login__container"  onSubmit={formik.handleSubmit}>
                 <DecoratedHeader styling={"login__header"} text="Zaloguj się"/>
                 <div className="login__inputs">
                     <label className="login__label" htmlFor="email">Email
@@ -18,21 +60,36 @@ export default function Login(){
                             className="login__field"
                             type="text"
                             name="email"
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.email}
+
                         />
                     </label>
+                    {formik.touched.email && formik.errors.email ? <p className="login__error">{formik.errors.email}</p> : null}
                     <label className="login__label" htmlFor="password">Hasło
                         <input
                             className="login__field"
                             type="password"
                             name="password"
+                            value={formik.values.password}
+                            onChange={event => {
+                                formik.handleChange(event);
+                                changePasswordlValue(event);
+                            }
+                            }
+                            onBlur={formik.handleBlur}
                         />
                     </label>
+                    {formik.touched.password && formik.errors.password ? <p className="login__error">{formik.errors.password}</p> : null}
                 </div>
                 <div className="login__buttons">
                     <Link className="login__button" to="/rejestracja">Załóż konto</Link>
                     <Link className="login__button" to="/">Zaloguj się</Link>
                 </div>
-            </div>
+                <button type="submit">submit</button>
+
+            </form>
         </section>
     )
 }
